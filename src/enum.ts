f7e30a2e96f3930d3819a9b8c5e15ad10797a004
NilsGke <n.j.goeke@gmail.com>
2025-08-07 13:34:34 +0200

    finish enum

--- src/enum.ts
+++ src/enum.ts
@@ -1,18 +1,32 @@
 import { ZodBaseClass } from "./base";
 import type { CheckResult } from "./types";
 
-class ZodEnum<T extends readonly [string, ...string[]]> extends ZodBaseClass<
+type ExcludeFromArray<
+  T extends readonly [...string[]],
+  K extends readonly [...string[]]
+> = Exclude<T[number], K[number]>[];
+
+type ExtractFromArray<
+  T extends readonly [...string[]],
+  K extends readonly [...string[]]
+> = Extract<T[number], K[number]>[];
+
+class ZodEnum<T extends readonly [...string[]]> extends ZodBaseClass<
   T[number]
 > {
+  private validStrings: Set<string>;
+
   constructor(validStrings: T) {
     super(
       (input): input is string => typeof input === "string",
       "input must be a string"
     );
 
+    this.validStrings = new Set(validStrings);
+
     this.addCheck({
       apply: (input: string): CheckResult<string> =>
-        validStrings.includes(input)
+        this.validStrings.has(input)
           ? { success: true, result: input }
           : {
               success: false,
@@ -23,12 +37,25 @@ class ZodEnum<T extends readonly [string, ...string[]]> extends ZodBaseClass<
     });
   }
 
+  exclude<const K extends readonly [...string[]]>(disallowedStrings: K) {
+    const newStrings = [...this.validStrings, ...disallowedStrings].filter(
+      (s) => !disallowedStrings.includes(s)
+    );
+    return _enum(newStrings as ExcludeFromArray<T, K>);
+  }
+
+  extract<const K extends readonly [...string[]]>(allowedStrings: K) {
+    const newStrings = [...this.validStrings].filter((s) =>
+      allowedStrings.includes(s)
+    ) as ExtractFromArray<T, K>;
+    return _enum(newStrings);
+  }
+
   // override parse and safeParse to make input accept strings instead of exact literal
   parse = (input: string) => super.parse(input as T[number]);
   safeParse = (input: string) => super.safeParse(input as T[number]);
 }
 
-const _enum = <const T extends [string, ...string[]]>(values: T) =>
-  new ZodEnum(values);
+const _enum = <const T extends [...string[]]>(values: T) => new ZodEnum(values);
 
 export default _enum;
--- tests/enum.test.ts
+++ tests/enum.test.ts
@@ -17,3 +17,33 @@ describe("z.enum()", () => {
     expect(z.enum(["abc", "def"]).parse("abc")).toBe("abc");
   });
 });
+
+describe("z.enum().exclude()", () => {
+  test("not in enum array", () => {
+    expectZodErrorMessage(
+      z.enum(["a", "b"]).exclude(["b"]).safeParse("b")
+    ).toMatch('string must be one of the following: "a"');
+  });
+  test("in enum array", () => {
+    expect(z.enum(["a", "b", "c"]).exclude(["a", "b"]).parse("c")).toBe("c");
+  });
+  test("exclude str that is not in enum array", () => {
+    expect(z.enum(["a", "b"]).exclude(["k"]).parse("a")).toBe("a");
+  });
+});
+
+describe("z.enum().extract()", () => {
+  test("not in enum array", () => {
+    expectZodErrorMessage(
+      z.enum(["a", "b"]).extract(["a"]).safeParse("b")
+    ).toMatch('string must be one of the following: "a"');
+  });
+  test("in enum array", () => {
+    expect(z.enum(["a", "b", "c"]).extract(["a", "b"]).parse("a")).toBe("a");
+  });
+  test("extract str that is not in enum array", () => {
+    expectZodErrorMessage(
+      z.enum(["a", "b"]).extract(["k"]).safeParse("a")
+    ).toMatch(/string must be one of the following: /);
+  });
+});
